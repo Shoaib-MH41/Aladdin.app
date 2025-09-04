b416570db6e4569277a84eae5223820b06bfe733
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-04 16:39:20 +0530

    Update mobile_page.dart

--- .github/workflows/lib/mobile_page.dart
+++ .github/workflows/lib/mobile_page.dart
@@ -1,1 +1,22 @@
+'package:flutter/material.dart';
+
+class MobilePage extends StatelessWidget {
+  const MobilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("📱 Mobile App"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: const Center(
+        child: Text(
+          "This is the Mobile App Page!",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
 

8c678c77d38bf70c3714375868de0dc17d9f1782
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-04 08:31:15 +0530

    Update mobile_page.dart

--- .github/workflows/lib/lib/mobile_page.dart
+++ .github/workflows/lib/lib/mobile_page.dart
@@ -1,1 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MobilePage extends StatelessWidget {
+  const MobilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Mobile App Options"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: const [
+          ListTile(
+            leading: Icon(Icons.android, color: Colors.green),
+            title: Text("Android OS"),
+          ),
+          ListTile(
+            leading: Icon(Icons.flutter_dash, color: Colors.blue),
+            title: Text("Flutter"),
+          ),
+          ListTile(
+            leading: Icon(Icons.code, color: Colors.deepOrange),
+            title: Text("Java"),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone_iphone, color: Colors.purple),
+            title: Text("iOS (Swift)"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 

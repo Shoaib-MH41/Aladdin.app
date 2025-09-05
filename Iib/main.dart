420a2342a8ee110ca94cc7e322dcb3d417533f8a
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-05 20:32:48 +0530

    Update main.dart

--- Iib/main.dart
+++ Iib/main.dart
@@ -1,1 +1,41 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Aladdin App Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Aladdin App")),
+      body: const Center(
+        child: Text(
+          "✨ Welcome to Aladdin Demo App ✨",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
+
+

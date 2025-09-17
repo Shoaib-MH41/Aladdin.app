2a2d0a313d4d6c7d67b3b6f5286034dd02eae0fe
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-17 09:54:15 +0530

    Create app_config.dart

---
+++ lib/src/models/app_config.dart
@@ -0,0 +1,15 @@
+class AppConfig {
+  final String appName;
+  final String appType;
+  final String language;
+  final String theme;
+  final String primaryColor;
+
+  AppConfig({
+    required this.appName,
+    required this.appType,
+    required this.language,
+    required this.theme,
+    required this.primaryColor,
+  });
+}

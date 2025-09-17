608a2ea537b3ea96ad20a18e60391dc5c5c7148d
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-17 09:08:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'src/features/splash/splash_screen.dart';
 
+void main() {
+  runApp(AladdinApp());
+}
+
+class AladdinApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'علادین ایپ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // پرائمری رنگ، بعد میں تبدیل کر سکتے ہو
+        fontFamily: 'Poppins', // فانٹ، pubspec.yaml میں شامل کرو
+      ),
+      home: SplashScreen(), // پہلی اسکرین اسپلاش ہوگی
+    );
+  }
+}

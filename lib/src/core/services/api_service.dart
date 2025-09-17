5bf3dcce2d97e325183bcecf2f30db88e053573c
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-17 09:48:46 +0530

    Create api_service.dart

---
+++ lib/src/core/services/api_service.dart
@@ -0,0 +1,17 @@
+class ApiService {
+  // GPT API سے کوڈ جنریٹ کرنے کا فنکشن (بعد میں اپ ڈیٹ کریں گے)
+  Future<String> generateCode({
+    required String appType,
+    required String language,
+    required String appName,
+    required String theme,
+  }) async {
+    // Placeholder: بعد میں GPT API کال ہوگی
+    return '''
+    // جنریٹ شدہ کوڈ کا نمونہ
+    void main() {
+      print('Hello, $appName in $language!');
+    }
+    ''';
+  }
+}

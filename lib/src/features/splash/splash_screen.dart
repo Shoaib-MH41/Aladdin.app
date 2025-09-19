f5e102f7fd50e8b6b80d760c292e1704f1da7285
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-19 11:11:55 +0530

    Update splash_screen.dart

--- lib/src/features/splash/splash_screen.dart
+++ lib/src/features/splash/splash_screen.dart
@@ -1,37 +1,52 @@
-import 'package:flutter/material.dart';
-import '../../core/constants/strings.dart';
-import '../../core/utils/navigation.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:aladdin_app/src/features/app_type_selection/app_type_selection_screen.dart';
+import 'package:aladdin_app/src/models/app_config.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNextScreen();
+  }
+
+  Future<void> _navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    
+    if (mounted) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const AppTypeSelectionScreen(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 3), () {
-      Navigation.pushReplacement(context, AppTypeSelectionScreen());
-    });
-
-    return Scaffold(
-      backgroundColor: Colors.blue[900],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.lightbulb,
-                size: 100,
-                color: Colors.white,
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(size: 100),
+            SizedBox(height: 20),
+            Text(
+              'Aladdin App',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 20),
-              Text(
-                AppStrings.appName,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 24,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );

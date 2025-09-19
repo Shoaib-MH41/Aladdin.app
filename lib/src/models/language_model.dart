b7f52583b05305b271d5d31cfd302c69f3552b93
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-19 21:15:44 +0530

    Create language_model.dart

---
+++ lib/src/models/language_model.dart
@@ -0,0 +1,13 @@
+class LanguageModel {
+  final String code;
+  final String name;
+  final String nativeName;
+  final String flagEmoji;
+
+  const LanguageModel({
+    required this.code,
+    required this.name,
+    required this.nativeName,
+    required this.flagEmoji,
+  });
+}

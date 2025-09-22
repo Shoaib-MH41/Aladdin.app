0d0a0723511a8a62cf57965afff131ca2c4ccbc5
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-22 23:16:19 +0530

    Delete lib/src directory

--- lib/src/core/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryColor = Colors.blue;
-  static const Color backgroundColor = Colors.white;
-  static const Color splashBackground = Color(0xFF0D47A1); // گہرا نیلا
-  static const Color cardColor = Colors.grey;
-  static const Color textColor = Colors.black87;
-}
--- lib/src/core/constants/strings.dart
+++
@@ -1,7 +0,0 @@
-class AppStrings {
-  static const String appName = 'علادین ایپ';
-  static const String startButton = 'شروع کریں';
-  static const String selectAppType = 'ایپ کی قسم منتخب کریں';
-  static const String selectLanguage = 'پروگرامنگ لینگویج منتخب کریں';
-  static const String generateApp = 'ایپ بنائیں';
-}

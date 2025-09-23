2ad8a1f139911e0b76cad7fdb747dac12e2cca91
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-24 00:20:50 +0530

    Update project_model.dart

--- lib/models/project_model.dart
+++ lib/models/project_model.dart
@@ -1,13 +1,37 @@
 class Project {
   final String id;
-  final String name;
-  final List<String> platforms;
-  final List<String> features;
+  String name;
+  String framework;
+  List<String> platforms;
+  Map<String, String> assets;
 
   Project({
     required this.id,
     required this.name,
+    required this.framework,
     required this.platforms,
-    required this.features,
+    required this.assets,
   });
+
+  // Convert to Map (for Firebase/GitHub/API use later)
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'framework': framework,
+      'platforms': platforms,
+      'assets': assets,
+    };
+  }
+
+  // Convert back from Map
+  factory Project.fromMap(Map<String, dynamic> map) {
+    return Project(
+      id: map['id'],
+      name: map['name'],
+      framework: map['framework'],
+      platforms: List<String>.from(map['platforms']),
+      assets: Map<String, String>.from(map['assets']),
+    );
+  }
 }

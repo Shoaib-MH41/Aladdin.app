4155efe52a1634693d2b15d1ad0709beac8b92e8
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-23 17:39:28 +0530

    Create project_model.dart

---
+++ lib/models/project_model.dart
@@ -0,0 +1,13 @@
+class Project {
+  final String id;
+  final String name;
+  final List<String> platforms;
+  final List<String> features;
+
+  Project({
+    required this.id,
+    required this.name,
+    required this.platforms,
+    required this.features,
+  });
+}

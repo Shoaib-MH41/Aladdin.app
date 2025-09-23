23f09cebcf3973cabe375fc09fe0c50febaf2024
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-23 06:26:50 +0530

    Update splash_screen.dart

--- lib/src/features/splash/splash_screen.dart
+++ lib/src/features/splash/splash_screen.dart
@@ -6,46 +6,37 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const LinearGradientBackground(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.auto_awesome, size: 80, color: Colors.white),
-            const SizedBox(height: 20),
-            const Text(
-              "Aladdin App",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/appType');
-              },
-              child: const Text("Get Started"),
-            )
-          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.purple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-    );
-  }
-}
-
-class LinearGradientBackground extends StatelessWidget {
-  const LinearGradientBackground({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.blue, Colors.purple],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(Icons.auto_awesome, size: 80, color: Colors.white),
+              const SizedBox(height: 20),
+              const Text(
+                "Aladdin App",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/appType');
+                },
+                child: const Text("Get Started"),
+              )
+            ],
+          ),
         ),
       ),
     );

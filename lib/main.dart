5931cff174ce60f40c9675248a08c8734ad901f3
Shoaib-MH41 <princeshoaib2122@gmail.com>
2025-09-24 22:29:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+
+// تمام اسکرینز import کریں
+import 'screens/home_screen.dart';
+import 'screens/project_screen.dart';
+import 'screens/selection_screen.dart';
+import 'screens/upload_screen.dart';
+import 'screens/chat_screen.dart';
+
+void main() {
+  runApp(const AladdinApp());
+}
+
 class AladdinApp extends StatelessWidget {
   const AladdinApp({super.key});
 
@@ -5,7 +18,8 @@ class AladdinApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aladdin App',
-      themeMode: ThemeMode.system, // ✅ device کے حساب سے auto
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system, // دن/رات auto
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -16,6 +30,7 @@ class AladdinApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         brightness: Brightness.dark,
       ),
+      // پہلا اسکرین
       initialRoute: '/home',
       routes: {
         '/home': (context) => const HomeScreen(),
